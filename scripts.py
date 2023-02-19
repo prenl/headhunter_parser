c33438d1d62cdcc92687f0e7f99133d70bcc4857
Yelnur <elnur.kazakhstan@gmail.com>
2023-02-19 19:10:38 +0600

    fix: get_average func

--- scripts.py
+++ scripts.py
@@ -1,24 +1,29 @@
 import pandas as pd
 
 
-def sort(dataframe, column: str) -> pd.DataFrame:
-    return dataframe.sort_values(by=column, ascending=True).reset_index(drop=True)
+def sort(dataframe, column: str, ascending: bool) -> pd.DataFrame:
+    return dataframe.sort_values(by=column, ascending=ascending).reset_index(drop=True)
 
 
 def get_min(dataframe, column: str) -> pd.DataFrame:
-    return pd.DataFrame(sort(dataframe, column).iloc[0]).reset_index(drop=True)
+    return pd.DataFrame(sort(dataframe, column, ascending=True).reset_index(drop=True).iloc[0])
 
 
 def get_max(dataframe, column: str) -> pd.DataFrame:
-    i = -1
-    l = list(sort(dataframe, column)[column])
-    while l[i] == "Unspecified":
-        i -= 1
-    return pd.DataFrame(sort(dataframe, column).iloc[i]).reset_index(drop=True)
+    i = 0
+    l = list(sort(dataframe, column, ascending=False)[column])
+    while l[i] == None:
+        i += 1
+    return pd.DataFrame(sort(dataframe, column, ascending=False).iloc[i]).reset_index(drop=True)
 
 
-def get_average(dataframe, column: str) -> float:
-    salary_list = list(int(i) for i in dataframe[column] if i!="Unspecified")
+def get_average(dataframe, column: str):
+    salary_list = []
+    for i in dataframe[column]:
+        try:
+            salary_list.append(int(i))
+        except Exception:
+            continue
     return sum(salary_list) / len(salary_list)
 
 
